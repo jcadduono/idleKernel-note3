223083a3e0b37394e8d2cb8cb3a935bdea2c3947
imoseyon <imoseyon@gmail.com>
2013-10-10 16:40:25 -0700

    Linux 3.4.1
    Conflicts:
    
    	drivers/mmc/core/cd-gpio.c
    	drivers/usb/host/xhci.h
    
    Conflicts:
    
    	arch/arm/boot/dts/tegra-cardhu.dts
    	arch/um/include/asm/pgtable.h

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1365,6 +1365,8 @@ struct xhci_bus_state {
 	u32			suspended_ports;
 	u32			port_remote_wakeup;
 	unsigned long		resume_done[USB_MAXCHILDREN];
+	/* which ports have started to resume */
+	unsigned long		resuming_ports;
 };
 
 static inline unsigned int hcd_index(struct usb_hcd *hcd)
@@ -1497,6 +1499,7 @@ struct xhci_hcd {
  * (16.66 ns x 5 = 84ns) ~100ns after writing to the PORTSC register.
  */
 #define XHCI_PORTSC_DELAY	(1 << 10)
+#define XHCI_TRUST_TX_LENGTH	(1 << 10)
 /*
  * In Synopsis DWC3 controller, XHCI RESET takes some time complete. If PIPE
  * RESET is not complete by the time USBCMD.RUN bit is set then HC fails to
