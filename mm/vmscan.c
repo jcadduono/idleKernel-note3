d1f63d153e12ba846421f5e80f8737910a41a853
imoseyon <imoseyon@gmail.com>
2014-01-26 20:24:57 -0800

    mm: fix merge errors

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1759,8 +1759,8 @@ out:
 		unsigned long scan;
 
 		scan = zone_nr_lru_pages(mz, lru);
-		if (priority || noswap || !vmscan_swappiness(mz, sc)) {
-			scan >>= priority;
+                if (sc->priority || noswap || !vmscan_swappiness(sc)) {
+                        scan >>= sc->priority;
 			if (!scan && force_scan)
 				scan = SWAP_CLUSTER_MAX;
 			scan = div64_u64(scan * fraction[file], denominator);
