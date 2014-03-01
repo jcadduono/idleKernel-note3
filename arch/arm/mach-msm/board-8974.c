222e2a5f749684aacf9a770f99a800f035efe7eb
imoseyon <imoseyon@gmail.com>
2014-03-01 08:47:19 -0800

    board-8974: enable ram console

--- arch/arm/mach-msm/board-8974.c
+++ arch/arm/mach-msm/board-8974.c
@@ -19,6 +19,7 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
+#include <linux/persistent_ram.h>
 #include <linux/memory.h>
 #include <linux/regulator/machine.h>
 #include <linux/regulator/krait-regulator.h>
@@ -320,11 +321,53 @@ static struct reserve_info msm8974_reserve_info __initdata = {
 	.paddr_to_memtype = msm8974_paddr_to_memtype,
 };
 
+#define PERSISTENT_RAM_BASE 0xbff00000
+#define PERSISTENT_RAM_SIZE SZ_1M
+#define RAM_CONSOLE_SIZE (124*SZ_1K * 2)
+
+#ifdef CONFIG_ANDROID_PERSISTENT_RAM
+static struct persistent_ram_descriptor pram_descs[] = {
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+        {
+                .name = "ram_console",
+                .size = RAM_CONSOLE_SIZE,
+        },
+#endif
+};
+
+static struct persistent_ram msm8974_persistent_ram = {
+        .start = PERSISTENT_RAM_BASE,
+        .size = PERSISTENT_RAM_SIZE,
+        .num_descs = ARRAY_SIZE(pram_descs),
+        .descs = pram_descs,
+};
+
+void __init add_persistent_ram(void)
+{
+    persistent_ram_early_init(&msm8974_persistent_ram);
+}
+#endif
+
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+static struct platform_device ram_console_device = {
+        .name = "ram_console",
+        .id = -1,
+};
+
+void __init add_ramconsole_devices(void)
+{
+    platform_device_register(&ram_console_device);
+}
+#endif /* CONFIG_ANDROID_RAM_CONSOLE */
+
 void __init msm_8974_reserve(void)
 {
 	reserve_info = &msm8974_reserve_info;
 	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8974_reserve_table);
 	msm_reserve();
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+	add_persistent_ram();
+#endif
 }
 
 static void __init msm8974_early_memory(void)
@@ -465,6 +508,9 @@ void __init msm8974_init(void)
 	samsung_sys_class_init();
 	msm_8974_init_gpiomux();
 	regulator_has_full_constraints();
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+        add_ramconsole_devices();
+#endif
 	board_dt_populate(adata);
 	msm8974_add_drivers();
 
