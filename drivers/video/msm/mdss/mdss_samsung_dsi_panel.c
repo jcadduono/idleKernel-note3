66b4ab04633544b896b1153e551e92db56925846
imoseyon <imoseyon@gmail.com>
2014-03-09 18:10:13 -0700

    mdss: add debugging to panel unknown event

--- drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_samsung_dsi_panel.c
@@ -3521,7 +3521,7 @@ static int samsung_dsi_panel_event_handler(int event)
 			break;
 #endif
 		default:
-			pr_err("%s : unknown event \n", __func__);
+			pr_info("[imoseyon] %s : unknown event %d\n", __func__, event);
 			break;
 
 	}
