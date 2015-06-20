49cbfc4c9207b2f45710d284786dc14688576972
imoseyon <imoseyon@gmail.com>
2015-06-20 10:03:53 -0700

    Linux 3.4.108

--- net/netfilter/xt_socket.c
+++ net/netfilter/xt_socket.c
@@ -299,13 +299,8 @@ xt_socket_get6_sk(const struct sk_buff *skb, struct xt_action_param *par)
 
 	} else if (tproto == IPPROTO_ICMPV6) {
 		if (extract_icmp6_fields(skb, thoff, &tproto, &saddr, &daddr,
-<<<<<<< HEAD
-					 &sport, &dport))
-			return NULL;
-=======
 					 &sport, &dport, &ipv6_var))
 			return false;
->>>>>>> cea9edd... netfilter: xt_socket: fix a stack corruption bug
 	} else {
 		return NULL;
 	}
