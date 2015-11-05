c3f97c27a442fc794a3adf10ff5ad68531505e70
James Christopher Adduono <jc@adduono.com>
2015-11-05 09:08:43 -0500

    Change all leanKernel references to idleKernel

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# leanKernel ramdisk rebuild script by jcadduono
-# This rebuild script is for Touchwiz Lollipop only
+# idleKernel for Samsung Galaxy Note 3 build script by jcadduono
+# This build script is for Touchwiz Lollipop only
 
 ################### BEFORE STARTING ################
 #
-# Run ./build_leanKernel.sh first to generate kernel zImage!
+# download a working toolchain and extract it somewhere and configure this file
+# to point to the toolchain's root directory.
+# I highly recommend Christopher83's Linaro GCC 4.9.x Cortex-A15 toolchain.
+# Download it here: http://forum.xda-developers.com/showthread.php?t=2098133
 #
-# Run this file to automatically rebuild the ramdisk and boot.img
-# with the contents in lk.ramdisk/ - used for testing ramdisk
-# modifications only. There is no need to run this file otherwise.
+# once you've set up the config section how you like it, you can simply run
+# ./build.sh
+# while inside the /idleKernel-note3/ directory.
 #
 ###################### CONFIG ######################
 
-# root directory of LeanKernel git repo (default is this script's location)
+# root directory of idleKernel git repo (default is this script's location)
 RDIR=$(pwd)
 
 [ -z $VARIANT ] && \
@@ -24,15 +27,15 @@ RDIR=$(pwd)
 #	tmo = N900T  (T-Mobile, same as Canadian)
 #	usc = N900R4 (US Cellular)
 #	vzw = N900V  (Verizon)
-VARIANT=tmo
+VARIANT=can
 
 [ -z $VER ] && \
 # version number
-VER="6.4"
+VER=6.5
 
-# kernel version string appended to 3.4.x-leanKernel-hlte-
+# kernel version string appended to 3.4.x-idleKernel-hlte-
 # (shown in Settings -> About device)
-KERNEL_VERSION="$VARIANT-$VER-jc"
+KERNEL_VERSION=$VARIANT-$VER
 
 [ -z $PERMISSIVE ] && \
 # should we boot with SELinux mode set to permissive? (1 = permissive, 0 = enforcing)
@@ -41,7 +44,7 @@ PERMISSIVE=0
 # output directory of flashable kernel
 OUT_DIR=$RDIR
 # output filename of flashable kernel
-OUT_NAME=leanKernel-hlte-$KERNEL_VERSION
+OUT_NAME=idleKernel-hlte-$KERNEL_VERSION
 
 # should we make a TWRP flashable zip? (1 = yes, 0 = no)
 MAKE_ZIP=1
@@ -49,15 +52,25 @@ MAKE_ZIP=1
 # should we make an Odin flashable tar.md5? (1 = yes, 0 = no)
 MAKE_TAR=1
 
+# directory containing cross-compile arm-cortex_a15 toolchain
+TOOLCHAIN=/home/jc/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
+
+# amount of cpu threads to use in kernel make process
+THREADS=4
+
 ############## SCARY NO-TOUCHY STUFF ###############
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-eabi-
+export LOCALVERSION=$KERNEL_VERSION
 
 if ! [ -f $RDIR"/arch/arm/configs/variant_hlte_"$VARIANT ] ; then
 	echo "Device variant/carrier $VARIANT not found in arm configs!"
 	exit -1
 fi
 
-if ! [ -d $RDIR"/lk.ramdisk/variant/$VARIANT/" ] ; then
-	echo "Device variant/carrier $VARIANT not found in lk.ramdisk/variant!"
+if ! [ -d $RDIR"/ik.ramdisk/variant/$VARIANT/" ] ; then
+	echo "Device variant/carrier $VARIANT not found in ik.ramdisk/variant!"
 	exit -1
 fi
 
@@ -67,13 +80,25 @@ KDIR=$RDIR/build/arch/arm/boot
 
 CLEAN_BUILD()
 {
+	echo "Cleaning build..."
+	cd $RDIR
+	rm -rf build
 	echo "Removing old boot.img..."
-	rm -f lk.zip/boot.img
+	rm -f ik.zip/boot.img
 	echo "Removing old zip/tar.md5 files..."
 	rm -f $OUT_DIR/$OUT_NAME.zip
 	rm -f $OUT_DIR/$OUT_NAME.tar.md5
-	echo "Removing old ramdisk structure..."
-	rm -rf build/ramdisk
+}
+
+BUILD_KERNEL()
+{
+	echo "Creating kernel config..."
+	cd $RDIR
+	mkdir -p build
+	make -C $RDIR O=build ik_defconfig \
+		VARIANT_DEFCONFIG=variant_hlte_$VARIANT
+	echo "Starting build..."
+	make -C $RDIR O=build -j"$THREADS"
 }
 
 BUILD_RAMDISK()
@@ -81,8 +106,8 @@ BUILD_RAMDISK()
 	echo "Building ramdisk structure..."
 	cd $RDIR
 	mkdir -p build/ramdisk
-	cp -ar lk.ramdisk/common/* build/ramdisk
-	cp -ar lk.ramdisk/variant/$VARIANT/* build/ramdisk
+	cp -ar ik.ramdisk/common/* build/ramdisk
+	cp -ar ik.ramdisk/variant/$VARIANT/* build/ramdisk
 	echo "Building ramdisk.img..."
 	cd $RDIR/build/ramdisk
 	mkdir -pm 755 dev proc sys system
@@ -102,13 +127,13 @@ BUILD_BOOT_IMG()
 		--pagesize 2048 \
 		--ramdisk_offset 0x02000000 \
 		--tags_offset 0x01E00000 \
-		--output $RDIR/lk.zip/boot.img 
+		--output $RDIR/ik.zip/boot.img 
 }
 
 CREATE_ZIP()
 {
 	echo "Compressing to TWRP flashable zip file..."
-	cd $RDIR/lk.zip
+	cd $RDIR/ik.zip
 	zip -r -9 - * > $OUT_DIR/$OUT_NAME.zip
 	cd $RDIR
 }
@@ -116,7 +141,7 @@ CREATE_ZIP()
 CREATE_TAR()
 {
 	echo "Compressing to Odin flashable tar.md5 file..."
-	cd $RDIR/lk.zip
+	cd $RDIR/ik.zip
 	tar -H ustar -c boot.img > $OUT_DIR/$OUT_NAME.tar
 	cd $OUT_DIR
 	md5sum -t $OUT_NAME.tar >> $OUT_NAME.tar
@@ -124,7 +149,7 @@ CREATE_TAR()
 	cd $RDIR
 }
 
-if CLEAN_BUILD && BUILD_RAMDISK && BUILD_BOOT_IMG; then
+if CLEAN_BUILD && BUILD_KERNEL && BUILD_RAMDISK && BUILD_BOOT_IMG; then
 	if [ $MAKE_ZIP -eq 1 ]; then CREATE_ZIP; fi
 	if [ $MAKE_TAR -eq 1 ]; then CREATE_TAR; fi
 	echo "Finished!"
