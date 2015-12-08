6a2e6dade3fb76b9ba3eba15fd088539a27e19b8
James Christopher Adduono <jc@adduono.com>
2015-12-08 02:18:13 -0500

    Revert "Revert to barcode_emul driver that works with KitKat IR interface"
    This reverts commit cb2978986359f88bd1cbd0348450beb1e7427265.

--- drivers/barcode_emul/barcode_emul_ice4_hlte.c
+++ drivers/barcode_emul/barcode_emul_ice4_hlte.c
@@ -49,6 +49,8 @@
 #include "barcode_emul_ice4_hlte.h"
 #include <linux/err.h>
 
+#define US_TO_PATTERN		1000000
+
 #if defined(CONFIG_MACH_H3GDUOS)
 #include <mach/gpiomux.h>
 #endif
@@ -88,10 +90,8 @@
 #define BOARD_REV02 2
 #define BOARD_REV03 3
 #define BOARD_REV07 3
-#define TIME_LIMIT_MSEC 300
-#define tm(time) (u32)ktime_to_us(time)
 
-extern unsigned int system_rev;
+extern int system_rev;
 
 struct barcode_emul_data {
 	struct i2c_client		*client;
@@ -853,10 +853,9 @@ static void ir_remocon_work(struct barcode_emul_data *ir_data, int count)
 	int ret;
 //	int sleep_timing;
 //	int end_data;
-	int actual_time;
+    int converting_factor = 1;
 	int emission_time;
 	int ack_pin_onoff;
-	ktime_t t1,t2;
 
 	if (count_number >= 100)
 		count_number = 0;
@@ -942,23 +941,12 @@ static void ir_remocon_work(struct barcode_emul_data *ir_data, int count)
 /*
 	printk(KERN_INFO "%s: sleep_timing = %d\n", __func__, sleep_timing);
 */
+    converting_factor = US_TO_PATTERN / data->ir_freq;
 	emission_time = \
-		(1000 * (data->ir_sum) / (data->ir_freq));
-/*	if (emission_time > 0)
+		((data->ir_sum) * (converting_factor) / 1000);
+	if (emission_time > 0)
 		msleep(emission_time);
-*/
-	actual_time = 0;
-	t1 = ktime_get();
-	while((gpio_get_value(g_pdata->irda_irq) == 0) && (actual_time <= emission_time))
-	{
-		int diff;
-		t2 = ktime_get();
-		diff = (tm(t2) - tm(t1))/1000;
-		msleep(10);
-		actual_time += 10;
-		if(diff > TIME_LIMIT_MSEC)
-			break;
-	}
+
 		pr_barcode("%s: emission_time = %d\n",
 					__func__, emission_time);
 
@@ -991,8 +979,8 @@ static ssize_t remocon_store(struct device *dev, struct device_attribute *attr,
 		const char *buf, size_t size)
 {
 	struct barcode_emul_data *data = dev_get_drvdata(dev);
-	unsigned int _data;
-	int count, i;
+	unsigned int _data, _tdata;
+	int count, i, converting_factor = 1;
 
 	pr_barcode("ir_send called\n");
 
@@ -1002,6 +990,7 @@ static ssize_t remocon_store(struct device *dev, struct device_attribute *attr,
 				break;
 			if (data->count == 2) {
 				data->ir_freq = _data;
+				converting_factor = US_TO_PATTERN / data->ir_freq;
 				if (data->on_off) {
 				//	msleep(30);
 				} else {
@@ -1016,12 +1005,13 @@ static ssize_t remocon_store(struct device *dev, struct device_attribute *attr,
 								= _data & 0xFF;
 				data->count += 3;
 			} else {
-				data->ir_sum += _data;
+				_tdata = _data / converting_factor;
+				data->ir_sum += _tdata;
 				count = data->count;
 				data->i2c_block_transfer.data[count]
-								= _data >> 8;
+								= _tdata >> 8;
 				data->i2c_block_transfer.data[count+1]
-								= _data & 0xFF;
+								= _tdata & 0xFF;
 				data->count += 2;
 			}
 
