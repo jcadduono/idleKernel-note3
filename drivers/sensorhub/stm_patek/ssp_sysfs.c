bc4122ce7ebe6a0e8be5d523f0203dc82b291e26
James Christopher Adduono <jc@adduono.com>
2015-12-10 02:31:58 -0500

    Add kconfig option to use legacy Sensorhub drivers

--- drivers/sensorhub/stm_patek/ssp_sysfs.c
+++ drivers/sensorhub/stm_patek/ssp_sysfs.c
@@ -80,6 +80,11 @@ static void enable_sensor(struct ssp_data *data,
 			proximity_open_lcd_ldi(data);
 			proximity_open_calibration(data);
 			set_proximity_threshold(data, data->uProxHiThresh, data->uProxLoThresh);
+
+#ifdef CONFIG_SENSORS_SSP_STM_LEGACY
+			input_report_abs(data->prox_input_dev, ABS_DISTANCE, 1);
+			input_sync(data->prox_input_dev);
+#endif
 		}
 #ifdef CONFIG_DUAL_LCD
 		else if ((iSensorType == GEOMAGNETIC_SENSOR)\
