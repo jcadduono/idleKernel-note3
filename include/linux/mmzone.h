0b00104f25f6e2e56392372a03f9c4cab792b999
James Christopher Adduono <jc@adduono.com>
2016-01-09 04:13:38 -0500

    Enable KSM and and support for UKSM (patch 0.1.2.3)

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -158,6 +158,9 @@ enum zone_stat_item {
 	NR_CMA_ACTIVE_FILE,
 	NR_CMA_UNEVICTABLE,
 #endif
+#ifdef CONFIG_UKSM
+	NR_UKSM_ZERO_PAGES,
+#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 /*
@@ -804,7 +807,7 @@ static inline int is_normal_idx(enum zone_type idx)
 }
 
 /**
- * is_highmem - helper function to quickly check if a struct zone is a 
+ * is_highmem - helper function to quickly check if a struct zone is a
  *              highmem zone or not.  This is an attempt to keep references
  *              to ZONE_{DMA/NORMAL/HIGHMEM/etc} in general code to a minimum.
  * @zone - pointer to struct zone variable
