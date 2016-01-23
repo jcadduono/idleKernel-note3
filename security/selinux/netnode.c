648efd15688a98ab6f96fca113d3fc15d2ce58bf
James Christopher Adduono <jc@adduono.com>
2016-01-23 16:17:01 -0500

    Merge SELinux changes from ik-touchwiz branch

--- security/selinux/netnode.c
+++ security/selinux/netnode.c
@@ -313,7 +313,7 @@ static __init int sel_netnode_init(void)
 	int iter;
 	int ret;
 
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	selinux_enabled = 1;
 #endif
 	if (!selinux_enabled)
