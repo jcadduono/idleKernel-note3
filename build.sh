d82338a8986daa4be596101d40661947f53ecd22
James Christopher Adduono <jc@adduono.com>
2016-01-23 20:51:12 -0500

    Set up branch as ik-nethunter-L, only for creating NetHunter kernel images

--- build.sh
+++ build.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 # idleKernel for Samsung Galaxy Note 3 build script by jcadduono
-# This build script is for AOSP/CyanogenMod with Kali Nethunter support only
+# This build script is for AOSP Lollipop with Kali Nethunter support only
 
 ################### BEFORE STARTING ################
 #
-# download a working toolchain and extract it somewhere and configure this file
-# to point to the toolchain's root directory.
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
 # I highly recommend Christopher83's Linaro GCC 4.9.x Cortex-A15 toolchain.
 # Download it here: http://forum.xda-developers.com/showthread.php?t=2098133
 #
 # once you've set up the config section how you like it, you can simply run
-# ./build.sh
-# while inside the /idleKernel-note3/ directory.
+# ./build.sh [VARIANT]
 #
-###################### CONFIG ######################
-
-# root directory of idleKernel git repo (default is this script's location)
-RDIR=$(pwd)
-
-[ -z $VARIANT ] && \
-# device variant/carrier, possible options:
+##################### VARIANTS #####################
+#
 #	att = N900A  (AT&T)
 #	can = N900W8 (Canadian, same as T-Mobile)
 #	eur = N9005  (Snapdragon International / hltexx / Europe)
@@ -32,31 +26,15 @@ RDIR=$(pwd)
 # japanese variants:
 #	dcm = N900D / SC-01F  (NTT Docomo)
 #	kdi = N900J / SCL22   (au by KDDI)
-VARIANT=can
+#
+###################### CONFIG ######################
 
-[ -z $VER ] && \
+# root directory of idleKernel git repo (default is this script's location)
+RDIR=$(pwd)
+
+[ $VER ] || \
 # version number
 VER=$(cat $RDIR/VERSION)
-
-# kernel version string appended to 3.4.x-idleKernel-hlte-
-# (shown in Settings -> About device)
-KERNEL_VERSION=$VARIANT-$VER-cm12.1-kali
-
-[ -z $PERMISSIVE ] && \
-# should we boot with SELinux mode set to permissive? (1 = permissive, 0 = enforcing)
-PERMISSIVE=1
-
-# output directory of flashable kernel
-OUT_DIR=$RDIR
-
-# output filename of flashable kernel
-OUT_NAME=idleKernel-hlte-$KERNEL_VERSION
-
-# should we make a TWRP flashable zip? (1 = yes, 0 = no)
-MAKE_ZIP=1
-
-# should we make an Odin flashable tar.md5? (1 = yes, 0 = no)
-MAKE_TAR=1
 
 # directory containing cross-compile arm-cortex_a15 toolchain
 TOOLCHAIN=/home/jc/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
@@ -66,21 +44,20 @@ THREADS=5
 
 ############## SCARY NO-TOUCHY STUFF ###############
 
+[ "$1" ] && {
+	VARIANT=$1
+} || {
+	VARIANT=can
+}
+
+[ -f "$RDIR/arch/arm/configs/variant_hlte_$VARIANT" ] || {
+	echo "Device variant/carrier $VARIANT not found in arm configs!"
+	exit 1
+}
+
 export ARCH=arm
 export CROSS_COMPILE=$TOOLCHAIN/bin/arm-eabi-
-export LOCALVERSION=$KERNEL_VERSION
-
-if ! [ -f $RDIR"/arch/arm/configs/variant_hlte_"$VARIANT ] ; then
-	echo "Device variant/carrier $VARIANT not found in arm configs!"
-	exit -1
-fi
-
-if ! [ -d $RDIR"/ik.ramdisk/variant/$VARIANT/" ] ; then
-	echo "Device variant/carrier $VARIANT not found in ik.ramdisk/variant!"
-	exit -1
-fi
-
-[ $PERMISSIVE -eq 1 ] && SELINUX="permissive" || SELINUX="enforcing"
+export LOCALVERSION="$VARIANT-nethunter-L-$VER"
 
 KDIR=$RDIR/build/arch/arm/boot
 
@@ -89,11 +66,6 @@ CLEAN_BUILD()
 	echo "Cleaning build..."
 	cd $RDIR
 	rm -rf build
-	echo "Removing old boot.img..."
-	rm -f ik.zip/boot.img
-	echo "Removing old zip/tar.md5 files..."
-	rm -f $OUT_DIR/$OUT_NAME.zip
-	rm -f $OUT_DIR/$OUT_NAME.tar.md5
 }
 
 BUILD_KERNEL()
@@ -103,57 +75,17 @@ BUILD_KERNEL()
 	mkdir -p build
 	make -C $RDIR O=build ik_defconfig \
 		VARIANT_DEFCONFIG=variant_hlte_$VARIANT
-	echo "Starting build..."
+	echo "Starting build for $VARIANT..."
 	make -C $RDIR O=build -j"$THREADS"
 }
 
-BUILD_RAMDISK()
+BUILD_DTB_IMG()
 {
-	VARIANT=$VARIANT $RDIR/setup_ramdisk.sh
-	cd $RDIR/build/ramdisk
-	echo "Building ramdisk.img..."
-	find | fakeroot cpio -o -H newc | gzip -9 > $KDIR/ramdisk.cpio.gz
-	cd $RDIR
+	echo "Generating dtb.img..."
+	$RDIR/scripts/dtbTool/dtbTool -o $KDIR/dtb.img $KDIR/ -s 2048
 }
 
-BUILD_BOOT_IMG()
-{
-	echo "Generating boot.img..."
-	$RDIR/scripts/mkqcdtbootimg/mkqcdtbootimg --kernel $KDIR/zImage \
-		--ramdisk $KDIR/ramdisk.cpio.gz \
-		--dt_dir $KDIR \
-		--cmdline "quiet console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=$SELINUX" \
-		--base 0x00000000 \
-		--pagesize 2048 \
-		--ramdisk_offset 0x02900000 \
-		--tags_offset 0x02700000 \
-		--output $RDIR/ik.zip/boot.img 
-}
+CLEAN_BUILD && BUILD_KERNEL && BUILD_DTB_IMG
 
-CREATE_ZIP()
-{
-	echo "Compressing to TWRP flashable zip file..."
-	cd $RDIR/ik.zip
-	zip -r -9 - * > $OUT_DIR/$OUT_NAME.zip
-	cd $RDIR
-}
+echo "Finished building $VARIANT!"
 
-CREATE_TAR()
-{
-	echo "Compressing to Odin flashable tar.md5 file..."
-	cd $RDIR/ik.zip
-	tar -H ustar -c boot.img > $OUT_DIR/$OUT_NAME.tar
-	cd $OUT_DIR
-	md5sum -t $OUT_NAME.tar >> $OUT_NAME.tar
-	mv $OUT_NAME.tar $OUT_NAME.tar.md5
-	cd $RDIR
-}
-
-if CLEAN_BUILD && BUILD_KERNEL && BUILD_RAMDISK && BUILD_BOOT_IMG; then
-	if [ $MAKE_ZIP -eq 1 ]; then CREATE_ZIP; fi
-	if [ $MAKE_TAR -eq 1 ]; then CREATE_TAR; fi
-	echo "Finished!"
-else
-	echo "Error!"
-	exit -1
-fi
--- build_nethunter_images.sh
+++ build_nethunter_images.sh
@@ -1,81 +1,30 @@
 #!/bin/bash
 # idleKernel for Samsung Galaxy Note 3 build script by jcadduono
-# This build script is for AOSP/CyanogenMod with Kali Nethunter support only
-# This build script builds all variants in /ik.ramdisk/variant/
+# This build script is for AOSP Lollipop with Kali Nethunter support only
+# This build script builds all variants in ./VARIANTS
 
 ################### BEFORE STARTING ################
 #
-# download a working toolchain and extract it somewhere and configure this file
-# to point to the toolchain's root directory.
+# download a working toolchain and extract it somewhere and configure
+# ./build.sh to point to the toolchain's root directory.
 # I highly recommend Christopher83's Linaro GCC 4.9.x Cortex-A15 toolchain.
 # Download it here: http://forum.xda-developers.com/showthread.php?t=2098133
-#
-# once you've set up the config section how you like it, you can simply run
-# ./build_all.sh
-# while inside the /idleKernel-note3/ directory.
 #
 ###################### CONFIG ######################
 
 # root directory of idleKernel git repo (default is this script's location)
 RDIR=$(pwd)
 
-[ -z $VER ] && \
-# version number
-VER=$(cat $RDIR/VERSION)
-
-# output directory of images
+# output directory of zImage and dtb.img
 OUT_DIR=/home/jc/build/kali-nethunter/nethunter-installer/kernels/lollipop
-
-# directory containing cross-compile arm-cortex_a15 toolchain
-TOOLCHAIN=/home/jc/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
-
-# amount of cpu threads to use in kernel make process
-THREADS=5
-
-SET_KERNEL_VERSION()
-{
-	# kernel version string appended to 3.4.x-idleKernel-hlte-
-	# (shown in Settings -> About device)
-	KERNEL_VERSION=$VARIANT-$VER-cm12.1-kali
-}
 
 ############## SCARY NO-TOUCHY STUFF ###############
 
-export ARCH=arm
-export CROSS_COMPILE=$TOOLCHAIN/bin/arm-eabi-
-
 KDIR=$RDIR/build/arch/arm/boot
-
-CLEAN_BUILD()
-{
-	echo "Cleaning build..."
-	cd $RDIR
-	rm -rf build
-	echo "Removing old image files..."
-	rm -rf $VARIANT_DIR
-}
-
-BUILD_KERNEL()
-{
-	echo "Creating kernel config..."
-	cd $RDIR
-	mkdir -p build
-	make -C $RDIR O=build ik_defconfig \
-		VARIANT_DEFCONFIG=variant_hlte_$VARIANT \
-		SELINUX_DEFCONFIG=selinux_never_enforce
-	echo "Starting build..."
-	make -C $RDIR O=build -j"$THREADS"
-}
-
-BUILD_DTB_IMG()
-{
-	echo "Generating dtb.img..."
-	$RDIR/scripts/dtbTool/dtbTool -o $KDIR/dtb.img $KDIR/ -s 2048
-}
 
 MOVE_IMAGES()
 {
-	echo "Moving images to $VARIANT_DIR/..."
+	echo "Moving zImage and dtb.img to $VARIANT_DIR/..."
 	rm -rf $VARIANT_DIR
 	mkdir $VARIANT_DIR
 	mv $KDIR/zImage $KDIR/dtb.img $VARIANT_DIR/
@@ -83,21 +32,11 @@ MOVE_IMAGES()
 
 mkdir -p $OUT_DIR
 
-for V in eur can kor dcm kdi spr
+for V in $(cat $RDIR/VARIANTS)
 do
-	VARIANT=$V
 	VARIANT_DIR=$OUT_DIR/hlte$V
-	SET_KERNEL_VERSION
-	export LOCALVERSION=$KERNEL_VERSION
-	if ! [ -f $RDIR"/arch/arm/configs/variant_hlte_"$VARIANT ] ; then
-		echo "Device variant/carrier $VARIANT not found in arm configs!"
-		continue
-	elif CLEAN_BUILD && BUILD_KERNEL && BUILD_DTB_IMG; then
-		MOVE_IMAGES
-	else
-		echo "Error!"
-		exit -1
-	fi
-done;
+	$RDIR/build.sh $V
+	MOVE_IMAGES
+done
 
-echo "Finished!"
+echo "Finished building NetHunter kernels!"
