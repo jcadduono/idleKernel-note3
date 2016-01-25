e6114d31f0f79d5505aaf41b911aa2a4f2b372ed
James Christopher Adduono <jc@adduono.com>
2016-01-25 15:30:25 -0500

    Update to N900TUVUFOL1 OSRC code

--- net/core/sock.c
+++ net/core/sock.c
@@ -1120,13 +1120,8 @@ static struct sock *sk_prot_alloc(struct proto *prot, gfp_t priority,
 	slab = prot->slab;
 	if (slab != NULL) {
 		sk = kmem_cache_alloc(slab, priority & ~__GFP_ZERO);
-		if (!sk) {
-			// ------------- START of KNOX_VPN ------------------//
-            sk->knox_uid = current->cred->uid;
-            sk->knox_pid = current->tgid;
-			// ------------- END of KNOX_VPN -------------------//
+		if (!sk)
 			return sk;
-		}
 		if (priority & __GFP_ZERO) {
 			if (prot->clear_sk)
 				prot->clear_sk(sk, prot->obj_size);
@@ -1147,8 +1142,8 @@ static struct sock *sk_prot_alloc(struct proto *prot, gfp_t priority,
 		sk_tx_queue_clear(sk);
 
 // ------------- START of KNOX_VPN ------------------//
-        sk->knox_uid = current->cred->uid;
-        sk->knox_pid = current->tgid;
+                sk->knox_uid = current->cred->uid;
+                sk->knox_pid = current->tgid;
 // ------------- END of KNOX_VPN -------------------//
 	}
 
@@ -1229,6 +1224,11 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 
 		sock_update_classid(sk);
 		sock_update_netprioidx(sk);
+
+// ------------- START of KNOX_VPN ------------------//
+            sk->knox_uid = current->cred->uid;
+            sk->knox_pid = current->tgid;
+// ------------- END of KNOX_VPN -------------------//
 
 	}
 
