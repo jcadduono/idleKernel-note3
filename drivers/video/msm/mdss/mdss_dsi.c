cebe5b90d72ef6e84f3d1c545e3f8cbde7a3e62d
lance.zhou <carck@foxmail.com>
2016-06-20 04:06:23 -0400

    drivers: Add state notifier driver big thanks to @neobuddy89 * Doze mode and FB notiers aren't friendly.   Introduce state notifier to make things smooth.
    Merged all latest changes and adapted to my mdss driver.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -23,6 +23,10 @@
 #include <linux/regulator/consumer.h>
 #include <linux/clk.h>
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 #include "mdss.h"
 #include "mdss_fb.h"
 #include "mdss_panel.h"
@@ -1474,6 +1478,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->mdp_tg_on = 1;
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			state_resume();
+#endif
 		break;
 	case MDSS_EVENT_BLANK:
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
@@ -1484,6 +1492,10 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata);
 		rc = mdss_dsi_off(pdata);
+#ifdef CONFIG_STATE_NOTIFIER
+		if (!use_fb_notifier)
+			state_suspend();
+#endif
 		break;
 #if defined(CONFIG_FB_MSM_MDSS_S6E8AA0A_HD_PANEL)
 	case MTP_READ:
